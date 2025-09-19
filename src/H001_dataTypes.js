f25d814e088ba86da50d59c575f2ef81a776bed8
Varthini-pvk <parvadhavarthini.k@gmail.com>
2025-09-19 22:46:36 +0530

    data-types-smaple

---
+++ src/H001_dataTypes.js
@@ -0,0 +1,7 @@
+console.log("Hello, JavaScript!");
+let my_name = "Parvadha Varthini K"
+let age = 33
+let address = `Chennai`
+
+
+console.log("name: " + my_name + ", age: " + age + ", address: " + address)
